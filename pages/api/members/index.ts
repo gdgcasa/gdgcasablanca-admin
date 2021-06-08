110e729f37fdc2c254f6238c82babb8cdf54574f
Omar Houmz <omarhoumz@gmail.com>
2021-06-08 23:08:36 +0100

    chore: add cors to members endpoint

--- pages/api/members/index.ts
+++ pages/api/members/index.ts
@@ -1,7 +1,20 @@
-import { getMembers } from '@/lib/admin-db'
 import { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
+
+import { getMembers } from '@/lib/admin-db'
+import initMiddleware from '@/lib/init-middlewares'
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  Cors({
+    origin: ['http://localhost:8000', /\.gdgcasablanca\.com$/],
+  }),
+)
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // Run cors
+  await cors(req, res)
+
   const members = await getMembers()
 
   res.status(200).json(members)
